9d6bfa78436f669079cb0540c3fd1ab17e2734d5
Shubham <shubhamp1251@gmail.com>
2022-03-31 14:52:25 +0530

    Second Changes

--- src/api.js
+++ src/api.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const request = axios.create({
     baseURL: "https://youtube.googleapis.com/youtube/v3/",
     params: {
-        key: process.env.REACT_APP_YOUTUBE_API_KEY
+        key: "AIzaSyB689GvGegdm79nObsPE1YAyomZ07H8KUY"
     }
 
 })
